997fb2c348e8c953fa2548deba8fc2cb015d3419
SheikhAttari007 <abdulhaseeb1926@gmail.com>
2024-10-26 19:21:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function inputValue(value){
+    var input = document.getElementById("input")
+    input.value += value;
+}
+function allClear(){
+    var input = document.getElementById("input")
+    input.value = ""
+}
+function result(){
+    var input = document.getElementById("input")
+    var ans = eval(input.value)
+    console.log("answer",ans)
+    input.value = ans 
+}
+
+function removeValue (){
+    var input = document.getElementById("input");
+    var copy = input.value.slice(0,-1)
+    console.log("copy",copy)
+    input.value = copy
+}
